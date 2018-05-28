07632bab33ee787035f2da5ecd88dd4ff1519fcc
lunastera <12947868+lunastera@users.noreply.github.com>
2018-05-28 09:45:27 +0900

    add .hyper.js

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 ln -s ~/dotfiles/.tmux ~/.tmux
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.zsh ~/.zsh
+ln -s ~/dotfiles/.hyper.js  ~/.hyper.js
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.hyper.js  ~/.hyper.js
 
